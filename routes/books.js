05336e424caa5490b9c31cb63203eac3f6902dcd
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-19 17:49:54 +0300

    some fixes

--- routes/books.js
+++ routes/books.js
@@ -7,52 +7,50 @@ const idLength = 8;
 /**
  * @swagger
  * components:
- *    schemas:
- *      Book:
- *          type: object
- *          required:
- *              - title
- *              - author
- *           properties:
- *              id:
- *                  type: string
- *                  description: The auto-generated id of the book.
- *              title:
- *                  type: string
- *                  description: The book title
- *              author:
- *                  type: string
- *                  description: The book author
- *              example:
- *                  id: d5fE_asz
- *                  title: The New Turing Omnibus
- *                  author: Alexander K. Dewdney
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
  */
-
 /**
  * @swagger
  * tags:
- *    name: Books
- *    description: The books managing API
+ *   name: Books
+ *   description: The books managing API
  */
 
 /**
  * @swagger
  * /books:
  *   get:
- *      summary: Returns the list of all the books
- *      tags: [Books]
- *      responses:
- *         200:
- *            description: The list of the books
- *            content:
- *               application/json:
- *                 schema:
- *                  type: array
- *                 items:
- *                   $ref: '#/components/schemas/Book'
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
  */
-
 router.get('/', (req, res) => {
   const books = req.app.db.get('books');
 
@@ -66,23 +64,22 @@ router.get('/', (req, res) => {
  *     summary: Get the book by id
  *     tags: [Books]
  *     parameters:
- *     - in: path
- *       name: id
- *       schema:
- *          type: string
- *       required: true
- *       description: The book id
- *   responses:
- *      200:
- *          description: The book description by id
- *          contents:
- *              application/json:
- *                  schema:
- *                      $ref: '#/components/schemas/Book'
- *      404:
- *          description: The book was not found
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: The book was not found
  */
-
 router.get('/:id', (req, res) => {
   const book = req.app.db.get('books').find({ id: req.params.id }).value();
 
@@ -96,26 +93,25 @@ router.get('/:id', (req, res) => {
 /**
  * @swagger
  * /books:
- *  post:
+ *   post:
  *     summary: Create a new book
  *     tags: [Books]
  *     requestBody:
  *       required: true
  *       content:
- *        application/json:
- *          schema:
- *           $ref: '#/components/schemas/Book'
- *     responses:
- *     200:
- *      description: The book was successfully created
- *      content:
  *         application/json:
- *            schema:
- *              $ref: '#/components/schemas/Book'
- *    500:
- *     description: Some server error
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The book was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
  */
-
 router.post('/', (req, res) => {
   try {
     const book = {
@@ -134,35 +130,34 @@ router.post('/', (req, res) => {
 /**
  * @swagger
  * /books/{id}:
- *   put:
+ *  put:
  *    summary: Update the book by the id
  *    tags: [Books]
  *    parameters:
- *    - in: path
- *      name: id
- *      schema:
- *        type: string
- *      required: true
- *      description: The book id
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
  *    requestBody:
- *       required: true
- *       content:
- *         application/json:
+ *      required: true
+ *      content:
+ *        application/json:
  *          schema:
- *          $ref: '#/components/schemas/Book'
+ *            $ref: '#/components/schemas/Book'
  *    responses:
- *       200:
+ *      200:
  *        description: The book was updated
  *        content:
- *         application/json:
- *           schema:
- *            $ref: '#/components/schemas/Book'
- *       404:
- *          description: The book was not found
- *       500:
- *         description: Some error happened
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Book'
+ *      404:
+ *        description: The book was not found
+ *      500:
+ *        description: Some error happened
  */
-
 router.put('/:id', (req, res) => {
   try {
     req.app.db
@@ -180,21 +175,22 @@ router.put('/:id', (req, res) => {
 /**
  * @swagger
  * /books/{id}:
- *  delete:
- *    summary: Remove the book by id
- *    tags: [Books]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *         type: string
- *        required: true
- *        description: The book id
- *   responses:
- *     200:
- *      description: The book was deleted
- *    404:
- *     description: The book was not found
+ *   delete:
+ *     summary: Remove the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *
+ *     responses:
+ *       200:
+ *         description: The book was deleted
+ *       404:
+ *         description: The book was not found
  */
 router.delete('/:id', (req, res) => {
   req.app.db.get('books').remove({ id: req.params.id }).write();
